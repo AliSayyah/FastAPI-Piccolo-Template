cbc372802fc6c696413c8aa7ced0f45a359e5075
Lint Action <lint-action@samuelmeuli.com>
2022-01-20 18:20:23 +0000

    Fix code style issues with Black

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ if __name__ == "__main__":
     import uvicorn
     from settings import RELOAD
 
-    kwargs = {
-        "reload": RELOAD,
-        "host": "0.0.0.0"
-    }
+    kwargs = {"reload": RELOAD, "host": "0.0.0.0"}
 
     uvicorn.run("app:app", **kwargs)
