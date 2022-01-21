e043096f41f48e463bd6b4cc27408bf6bc1a433b
ali <ali.sayyah2@gmail.com>
2022-01-21 15:08:05 +0330

    added default values for environment variables

--- settings.py
+++ settings.py
@@ -1,25 +1,31 @@
 from starlette.config import Config
 
+
+def generate_secret_key():
+    import secrets
+
+    return secrets.token_urlsafe(32)
+
+
 # Configuration from environment variables or '.env' file.
 config = Config(".env")
 
 # main database
-DB_NAME = config("DB_NAME")
-DB_USER = config("DB_USER")
-DB_PASSWORD = config("DB_PASSWORD")
-DB_HOST = config("DB_HOST")
-DB_PORT = config("DB_PORT")
+DB_NAME = config("DB_NAME", cast=str, default="main")
+DB_USER = config("DB_USER", cast=str, default="postgres")
+DB_PASSWORD = config("DB_PASSWORD", cast=str, default="postgres")
+DB_HOST = config("DB_HOST", cast=str, default="db")
+DB_PORT = config("DB_PORT", cast=int, default=5432)
 
 # test database
-DB_TEST_USER = config("DB_TEST_USER")
-DB_TEST_PASSWORD = config("DB_TEST_PASSWORD")
-DB_TEST_NAME = config("DB_TEST_NAME")
+DB_TEST_USER = config("DB_TEST_USER", cast=str, default="postgres")
+DB_TEST_PASSWORD = config("DB_TEST_PASSWORD", cast=str, default="postgres")
+DB_TEST_NAME = config("DB_TEST_NAME", cast=str, default="test")
 DB_TEST_HOST = DB_HOST
 DB_TEST_PORT = DB_PORT
 
-
-SECRET_KEY = config("SECRET_KEY")
-RELOAD = config("RELOAD")
+SECRET_KEY = config("SECRET_KEY", default=generate_secret_key())
+RELOAD = config("RELOAD", cast=bool, default=True)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 ORIGINS = [
