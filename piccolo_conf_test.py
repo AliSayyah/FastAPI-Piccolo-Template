6d6c605965be07d941fbb7036244a4b99ac41b92
Lint Action <lint-action@samuelmeuli.com>
2022-01-21 11:28:41 +0000

    Fix code style issues with Black

--- piccolo_conf_test.py
+++ piccolo_conf_test.py
@@ -1,6 +1,12 @@
 from piccolo_conf import *  # noqa
 
-from settings import DB_TEST_NAME, DB_TEST_USER, DB_TEST_PASSWORD, DB_TEST_HOST, DB_TEST_PORT
+from settings import (
+    DB_TEST_NAME,
+    DB_TEST_USER,
+    DB_TEST_PASSWORD,
+    DB_TEST_HOST,
+    DB_TEST_PORT,
+)
 
 DB = PostgresEngine(
     config={
