9eac34b5b1da1853738428cf01f6474a69c84064
ali <ali.sayyah2@gmail.com>
2022-01-21 14:58:19 +0330

    added test action for ci/cd

--- piccolo_conf_test.py
+++ piccolo_conf_test.py
@@ -1,12 +1,13 @@
 from piccolo_conf import *  # noqa
 
+from settings import DB_TEST_NAME, DB_TEST_USER, DB_TEST_PASSWORD, DB_TEST_HOST, DB_TEST_PORT
 
 DB = PostgresEngine(
     config={
-        "database": "piccolo_project_test",
-        "user": "postgres",
-        "password": "",
-        "host": "localhost",
-        "port": 5432,
+        "database": DB_TEST_NAME,
+        "user": DB_TEST_USER,
+        "password": DB_TEST_PASSWORD,
+        "host": DB_TEST_HOST,
+        "port": DB_TEST_PORT,
     }
 )
--- settings.py
+++ settings.py
@@ -2,11 +2,22 @@ from starlette.config import Config
 
 # Configuration from environment variables or '.env' file.
 config = Config(".env")
+
+# main database
 DB_NAME = config("DB_NAME")
 DB_USER = config("DB_USER")
 DB_PASSWORD = config("DB_PASSWORD")
 DB_HOST = config("DB_HOST")
 DB_PORT = config("DB_PORT")
+
+# test database
+DB_TEST_USER = config("DB_TEST_USER")
+DB_TEST_PASSWORD = config("DB_TEST_PASSWORD")
+DB_TEST_NAME = config("DB_TEST_NAME")
+DB_TEST_HOST = DB_HOST
+DB_TEST_PORT = DB_PORT
+
+
 SECRET_KEY = config("SECRET_KEY")
 RELOAD = config("RELOAD")
 ALGORITHM = "HS256"
