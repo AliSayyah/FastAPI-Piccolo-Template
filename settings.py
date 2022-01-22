5cd92450eb1dfe5832a959e22e861f35852f91ec
ali <ali.sayyah2@gmail.com>
2022-01-22 21:52:03 +0330

    check config

--- settings.py
+++ settings.py
@@ -11,22 +11,21 @@ def generate_secret_key():
 config = Config(".env")
 
 # main database
-DB_NAME = config("DB_NAME", cast=str, default="postgres")
-DB_USER = config("DB_USER", cast=str, default="postgres")
-DB_PASSWORD = config("DB_PASSWORD", cast=str, default="postgres")
-DB_HOST = config("DB_HOST", cast=str, default="db")
-DB_PORT = config("DB_PORT", cast=int, default=5432)
+DB_NAME = config("DB_NAME", default="postgres")
+DB_USER = config("DB_USER", default="postgres")
+DB_PASSWORD = config("DB_PASSWORD", default="postgres")
+DB_HOST = config("DB_HOST", default="db")
+DB_PORT = config("DB_PORT", default=5432)
 
 # test database
-DB_TEST_USER = config("DB_TEST_USER", cast=str, default="postgres")
-DB_TEST_PASSWORD = config("DB_TEST_PASSWORD", cast=str, default="postgres")
-DB_TEST_NAME = config("DB_TEST_NAME", cast=str, default="test")
-DB_TEST_HOST = config("DB_TEST_HOST", cast=str, default=DB_HOST)
-DB_TEST_PORT = config("DB_TEST_PORT", cast=int, default=DB_PORT)
-
+DB_TEST_USER = config("DB_TEST_USER", default="postgres")
+DB_TEST_PASSWORD = config("DB_TEST_PASSWORD", default="postgres")
+DB_TEST_NAME = config("DB_TEST_NAME", default="test")
+DB_TEST_HOST = config("DB_TEST_HOST", default=DB_HOST)
+DB_TEST_PORT = config("DB_TEST_PORT", default=DB_PORT)
 
 SECRET_KEY = config("SECRET_KEY", default=generate_secret_key())
-RELOAD = config("RELOAD", cast=bool, default=True)
+RELOAD = config("RELOAD", default=True)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 ORIGINS = [
