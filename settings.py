f65073934da5ef3a6ab48f5a102e6fdc983f6f63
ali sayyah <ali.sayyah2@gmail.com>
2022-01-22 20:40:41 +0330

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from starlette.config import Config
 
 def generate_secret_key():
     import secrets
-
+       
     return secrets.token_urlsafe(32)
 
 
@@ -21,8 +21,9 @@ DB_PORT = config("DB_PORT", cast=int, default=5432)
 DB_TEST_USER = config("DB_TEST_USER", cast=str, default="postgres")
 DB_TEST_PASSWORD = config("DB_TEST_PASSWORD", cast=str, default="postgres")
 DB_TEST_NAME = config("DB_TEST_NAME", cast=str, default="test")
-DB_TEST_HOST = DB_HOST
-DB_TEST_PORT = DB_PORT
+DB_TEST_HOST = config("DB_TEST_HOST", cast=str, default=DB_HOST)
+DB_TEST_PORT = config("DB_TEST_PORT", cast=int, default=DB_PORT)
+
 
 SECRET_KEY = config("SECRET_KEY", default=generate_secret_key())
 RELOAD = config("RELOAD", cast=bool, default=True)
