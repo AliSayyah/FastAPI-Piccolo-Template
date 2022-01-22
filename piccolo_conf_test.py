455463b2ad1ebe82e490a8d92336de80ad715cb2
ali sayyah <ali.sayyah2@gmail.com>
2022-01-22 21:17:45 +0330

    Update piccolo_conf_test.py

--- piccolo_conf_test.py
+++ piccolo_conf_test.py
@@ -7,13 +7,14 @@ from settings import (
     DB_TEST_HOST,
     DB_TEST_PORT,
 )
-
-DB = PostgresEngine(
-    config={
+config={
         "database": DB_TEST_NAME,
         "user": DB_TEST_USER,
         "password": DB_TEST_PASSWORD,
         "host": DB_TEST_HOST,
         "port": DB_TEST_PORT,
     }
+print(config)
+DB = PostgresEngine(
+    config
 )
