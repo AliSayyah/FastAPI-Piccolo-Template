0144b1dae0877e3e0d6d86b11501c2cb6a19e7be
ali <ali.sayyah2@gmail.com>
2022-01-22 21:43:18 +0330

    check config

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -3,21 +3,22 @@ from piccolo.engine.postgres import PostgresEngine
 
 from settings import DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER
 
+config = {
+    "database": DB_NAME,
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "host": DB_HOST,
+    "port": DB_PORT,
+}
+print(config)
 DB = PostgresEngine(
-    config={
-        "database": DB_NAME,
-        "user": DB_USER,
-        "password": DB_PASSWORD,
-        "host": DB_HOST,
-        "port": DB_PORT,
-    }
+    config
 )
 
 APP_REGISTRY = AppRegistry(
     apps=[
         "home.piccolo_app",
         "piccolo_admin.piccolo_app",
-        "augmented_reality.piccolo_app",
         "accounts.piccolo_app",
     ]
 )
